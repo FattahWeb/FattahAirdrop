9da024e6ba7642387ddae1a066400748718f19c1
FattahWeb <hdulfattah@.com>
2024-11-12 10:51:30 +0700

    Pesan commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Menambahkan event listener pada tombol
+document.getElementById("pawButton").addEventListener("click", function() {
+    // Mengarahkan pengguna ke URL yang diinginkan
+    window.location.href = "https://t.me/PAWSOG_bot/PAWS?startapp=4LBz66Ak"; // Ganti dengan URL tujuan Anda
+  });
+  
+ document.getElementById("blumButton").addEventListener("click", function() {
+      // Mengarahkan pengguna ke URL yang diinginkan
+      window.location.href = "https://t.me/blum/app?startapp=ref_9KqNxxDXFn"; // Ganti dengan URL tujuan Anda
+    });
+
+ document.getElementById("nodeButton").addEventListener("click", function() {
+      // Mengarahkan pengguna ke URL yang diinginkan
+      window.location.href = "https://app.nodepay.ai/register?ref=kll1guldtqSsh2y"; // Ganti dengan URL tujuan Anda
+    });
+
+ document.getElementById("garButton").addEventListener("click", function() {
+      // Mengarahkan pengguna ke URL yang diinginkan
+      window.location.href = "https://app.gradient.network/signup?code=5VPKM7"; // Ganti dengan URL tujuan Anda
+    });
+
+ document.getElementById("bmhButton").addEventListener("click", function() {
+      // Mengarahkan pengguna ke URL yang diinginkan
+      window.location.href = "https://app.blockmesh.xyz/register?invite_code=ad369300-f7e5-4e0f-8a3d-50954bf1296e"; // Ganti dengan URL tujuan Anda
+    });
